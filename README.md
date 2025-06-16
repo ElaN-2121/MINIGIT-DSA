3ecd7447d1f3bc5546806c174d823eac5c3ea47a
elnathan <elnathannigussie@gmail.com>
2025-06-16 18:46:57 +0300

    Additional headers added and main function defined

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MINIGIT-DSA
 An implementation of a lightweight version of Git that tracks changes to files, supports commits, branching, merging, and log history, without using external version control libraries.
+
+### External Code Used
+**SHA-1 Hashing**: This project uses a public domain SHA-1 implementation from(https://github.com/vog/sha1/blob/master/sha1.hpp) to stimulate Git's file tracking logic.
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Init.h"
+#include "add.h" 
+#include "Commit.h"
+#include "Blob.h"
+#include "Branch.h"
+#include "Checkout.h"
+#include "Merge.h"
+#include "Reference.h"
+#include "StagingArea.h"
+
+using namespace std;
+
+int main(){
+    string cmd;
+    while (true){
+        cout<<"#";
+        getline(cin, cmd);
+
+        if(cmd=="init"){
+
+        }
+        else if(cmd=="exit"){
+            cout<<"Leaving MiniGit....\n";
+            break;
+        }else{
+            cout<<"Unknown command "<<cmd<< " has been entered"<<endl;
+        }
+    }
+    return 0;
+}
