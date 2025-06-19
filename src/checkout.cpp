6782fc7bf1a1252858dd3cb3bef6c2087351afaa
zafran-seid <zafranseid35@gmail.com>
2025-06-19 12:54:45 -0700

    Update checkout.cpp

--- src/checkout.cpp
+++ src/checkout.cpp
@@ -1,1 +1,19 @@
 #include "../include/Checkout.h"
+#include "commitDatabase.h"
+#include <iostream>
+
+void checkoutCommit(const std::string& hash){
+  auto it=commitDatabase.find(hash);
+  if (it==commitDatabase.end()){
+    std::cout<<"Error: No commit  found!!\n";
+    return;
+  }
+  const Commit& c=it->second;
+  HEAD = c.hash;
+  std::cout<< "Checkedout Commit "<< c.hash <<"\n";
+  std::cout<<"Restored Files:\n";
+  for (const auto& file :c.files){
+    std::cout<<" "<< file <<": "<<c.fileContents.at(file)<<"\n";
+  }
+  std::cout <<"\n HEAD is now at "<<HEAD<<\n;
+} 
