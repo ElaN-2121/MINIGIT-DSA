15b80ca6b35a0634d6c1d9ae5b924586e03ddd8b
ElaN-2121 <elnathannigussie@gmail.com>
2025-06-20 15:28:53 +0300

    Update commit.cpp

--- src/commit.cpp
+++ src/commit.cpp
@@ -1,4 +1,5 @@
 #include "../include/Commit.h"
+#include "../StagingArea.h"
 #include <iostream>
 #include <fstream>
 #include <filesystem>
@@ -6,26 +7,33 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-void commit(const string& message){
-    time_t now= time(0);
+void commit(const string& message, StagingArea& staging){
+    time_t ow=time(0);
     string commit_id = to_string(now);
-    string commit_folder= "vcs/commits/" + commit_id;
+    string commit_folder= ".minigit/commits/" + commit_id;
     fs::create_directories(commit_folder);
-    
-    string staging_folder="vcs/staging";
-    if(!fs::exists(staging_folder) || fs::is_empty(staging_folder)){
-        cout<<"No staged files to commit.\n";
+
+    const auto& stagedFiles = stagingArea.getStagedFiles();
+    if(stagedFiles.empty()){
+        std::cot<<"No staged files to commit.\n";
         return;
     }
-    for(const auto& entry : fs::directory_iterator(staging_folder)){
-        fs::copy_file(entry.path(),commit_folder + "/" + entry.path().filename().string(), fs::copy_options::overwrite_existing);
+    for (const auto& [filename, hash] : stagedFiles){
+        std::string blobPath, std::ios::binary);
+        if(!in) {
+            std::cerr<<"error: Blob "<<hash<<" for file "<<filename<<" not found.\n";
+            continue;
+        }
+        std::ofstream out (commit_folder + "/" + filename, std::ios::binary);
+        out<<in.rdbuf();
+        in.close();
+        out.close();
     }
-    ofstream log("vcs/log.txt", ios::app);
+    
+    std::ofstream log(".minigit/log.txt", std::ios::app);
     log<<"Commit: "<<commit_id << "\n";
     log<<"Message: "<<message<< "\n";
-
-    char* dt= ctime(&now);
-    log<< "Date: "<< dt;
+    log<< "Date: "<< dctime(&now;
     log<<"-----------------------\n";
     log.close();
     cout<<"Committed successfully with ID "<< commit_id<<endl;
