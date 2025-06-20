ba4ea3ccbf271a4940ec3635f3191e324ac8651f
imanbelete <iman.ugr-8533-16@aau.edu.et>
2025-06-20 23:37:10 +0800

    Updated log.cpp

--- src/log.cpp
+++ src/log.cpp
@@ -1,26 +1,18 @@
-#include "../include/Log.h"
-#include "commitDatabase.h"
 #include <iostream>
 #include <fstream>
-#include "Utils.h"
+#include <string>
 
-void printLog(const std::string& headHash){
-  std::string current=headHash;
-  while (!current.empty()){
-    std::ifstream fin(".minigit/commits/" + current+"/meta.txt");
-    if (!fin.is_open()) break;
-
-    std::string line, hash, date, message, parent;
-    while (getline(fin,line)){
-      if(line.rfind("hash",0)==0) hash=trim(line.substr(5));
-      else if (line.rfind("timestamp",0)==0) date=trim(line.substr(10));
-      else if (line.rfind("message",0)==0) message=trim(line.substr(8));
-      else if (line.rfind("parent",0)==0) parent=trim(line.substr(7));    
+void printLog(const std::string& headHash) {
+    std::ifstream logFile(".minigit/log.txt");
+    if (!logFile) {
+        std::cerr << "Could not open log file.\n";
+        return;
     }
-    std::cout<<"Commit: "<<hash<<"\n";
-    std::cout<<"Date: "<<date<<"\n";
-    std::cout<<"Message: "<<message<<"\n\n";
-    if(parent.empty()||parent=="none")break;
-    current=parent;
-  }
+
+    std::string line;
+    while (std::getline(logFile, line)) {
+        std::cout << line << std::endl;
+    }
+
+    logFile.close();
 }
