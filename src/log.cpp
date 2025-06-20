9b60aef955c44f056d99495ea00b94701e826aa7
ElaN-2121 <elnathannigussie@gmail.com>
2025-06-20 15:43:16 +0300

    Update log.cpp

--- src/log.cpp
+++ src/log.cpp
@@ -1,15 +1,25 @@
 #include "../include/Log.h"
 #include "commitDatabase.h"
 #include <iostream>
+#include <fstream>
 
 
 void printLog(const std::string& headHash){
   std::string current=headHash;
   while (!current.empty()){
-    const Commit& c=commitDatabase[current];
-    std::cout<<"Commit: "<<c.hash<<"\n";
-    std::cout<<"Date: "<<c.timestamp<<"\n";
-    std::cout<<"Message: "<<c.message<<"\n";
-    current=c.parent;
+    std::ifstream fin(".minigit/commits/" + current);
+    if (!fin.is_open()) break;
+
+    std::string line, hash, date, message, parent;
+    while (getline(fin,line)){
+      if(line.rfind("hash",0)==0) hash=line.substr(5);
+      else if (line.rfind("timestamp",0)==0) date=line.substr(10);
+      else if (line.rfind("message",0)==0) date=line.substr(8);
+      else if (line.rfind("parent",0)==0) date=line.substr(7);    
+    }
+    std::cout<<"Commit: "<<hash<<"\n";
+    std::cout<<"Date: "<<timestamp<<"\n";
+    std::cout<<"Message: "<<message<<"\n\n";
+    current=parent;
   }
 }
